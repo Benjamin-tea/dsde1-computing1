7f3ef419a88280a6bb30463ddc8a837739148642
unknown <bjs19@ic.ac.uk>
2019-10-20 20:33:43 +0100

    adding functions project

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -9,20 +9,22 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
-    print(a)
+    ad1 = a + 1
+    print(ad1)
 
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
+    ad2 = a + 1
+    return ad2# hint this is incomplete
 
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    return (a + b)
 
 
 # write a function that takes two
@@ -30,14 +32,16 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    cmbo = sum(a, b)
+    return inc_return(cmbo)
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    fact_check = bool((a % 2) == 0)
+    return fact_check
 
 
 # create for loop that takes a string
@@ -49,5 +53,5 @@ def is_even(a):
 def string_repeat(phrase, repeat):
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+    return phrase * repeat
 
