168363f700d1c30629b9d294f3cfa45aa3d2be23
Benjamin-tea <56298574+Benjamin-tea@users.noreply.github.com>
2019-10-22 11:29:12 +0100

    Update pendulum.py

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -7,10 +7,8 @@ def period(L, g):
     if isinstance(g, (int, float)) == False:
         print(ii)
         return
-    if g == 0:
+    if g == 0 or (L / g) < 0:
         print(ii)
         return
-    if (L / g) < 0:
-        print(ii)
     T = (2 * math.pi) * ((L / g) ** 0.5) 
     return T
